2d2d112fcb5f5477d459a919d63a5791519002f7
Filip Kahm <fika0302@storsjogymnasiet.se>
2023-03-20 14:03:48 +0100

    personnummer

---
+++ getpnr.py
@@ -0,0 +1,27 @@
+import requests
+import random
+looping = True
+
+
+
+while looping:
+
+    
+    slumpnr = random.randint(1, 30000)
+
+
+    url =f"https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763/json?_offset={slumpnr}&_limit=1"
+
+    req = requests.get(url)
+    json_data = req.json()
+    list_results = json_data['results']
+    personnummer = list_results[0]['testpersonnummer']
+
+
+
+    print("\n personnummer:" +personnummer)
+
+    svar = input("vill ni slumpa ett personnummer till? j/n\n")
+
+    if (svar == "n"):
+        break
